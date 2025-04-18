c5f43cbabfd0b0f89ccea46cb8510622215a1ec6
bsnyder70 <91572657+bsnyder70@users.noreply.github.com>
2025-04-17 23:44:10 -0400

    Little more tokenization in preparation for more

--- src/data_process.py
+++ src/data_process.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 import pandas as pd
 import kagglehub
@@ -32,15 +33,17 @@ def build_data():
     df.rename(columns={'v1': 'labels', 'v2': 'examples'}, inplace=True)
     df['labels'] = df['labels'].map({'ham': 0, 'spam': 1})
 
+
     print(df.head(5))
 
+    # Need to go through all the vocabulary, map to integers, calculate the size of vocab, add other tokens (cls, etc)
+    
     # clean examples and tokenize as per assignment 3
     df['examples'] = df['examples'].apply(preprocess_text)
 
-    labels = df['labels'].values
-    examples = df['examples'].values
-
-    print(df.head(5))
+    
+    labels = torch.tensor(df['labels'].values)
+    examples = torch.tensor(df['examples'].values)
 
     spam_dataset = SpamDataset(labels, examples)
 
@@ -59,8 +62,18 @@ def preprocess_text(text):
     text = text.lower()
 
     words = text.split(" ")
+    
+    # VERY crude tokenization and padding
+    MAX_SEQ_LEN = 180
+    words.insert(0, "<cls>")
+    words.append("<eos>")
 
+    for i in range(len(words), MAX_SEQ_LEN):
+        words.append("<pad>")
+    
     return words
+
+
 
 
 build_data()
